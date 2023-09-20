5e938069269dfac828e09d12b818076460e3dc04
Romain <116175483+r0om1ain@users.noreply.github.com>
2023-09-20 17:38:00 +0200

    add doc for projetc

--- js/main.js
+++ js/main.js
@@ -56,20 +56,6 @@
             $(this).css("width", $(this).attr("aria-valuenow") + '%');
         });
     }, {offset: '80%'});
-
-
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
-
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
-    
     
     // Back to top button
     $(window).scroll(function () {
